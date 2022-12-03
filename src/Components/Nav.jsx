e3dbda1f0bf308ac43f297fc97d8ab7c391e9d01
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-03 15:06:51 -0500

    customize hamburger menu and made responsive for mobile

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,7 @@
 import "./Styles/Nav.css"
-import { push as Menu } from 'react-burger-menu'
+import { stack as Menu } from 'react-burger-menu'
 import { NavLink } from "react-router-dom"
-// import { TfiAlignJustify } from "react-icons/tfi"
+
 
 
 
@@ -10,17 +10,21 @@ function Nav() {
 
   return (
     <div className="nav-bar-component">
-      <div className="nav">
+      <div className="nav-desktop">
+            <NavLink className="navlink" to="/">Home</NavLink>
+            <NavLink className="navlink" to="/about">About</NavLink>
+            <NavLink className="navlink" to="/projects">Projects</NavLink>
+            <NavLink className="navlink" to="/contact">Contact</NavLink>
+      </div> 
+
+      <div className="nav-mobile">
         <Menu>
-        <NavLink className="navlink" to="/">Home</NavLink>
-        <NavLink className="navlink" to="/about">About</NavLink>
-        <NavLink className="navlink" to="/projects">Projects</NavLink>
-        <NavLink className="navlink" to="/contact">Contact</NavLink>
+            <NavLink className="navlink" to="/">Home</NavLink>
+            <NavLink className="navlink" to="/about">About</NavLink>
+            <NavLink className="navlink" to="/projects">Projects</NavLink>
+            <NavLink className="navlink" to="/contact">Contact</NavLink>
           </Menu>
       </div> 
-     
-      
-      
       </div>
   )
 }
