0290710c769a5b2dfb39a2ac8d677bbdbb80af88
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-04 18:07:37 -0500

    add more animations

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,14 +1,84 @@
-import "../App.css"
-import headshot from "../images/headshot.jpeg"
+import "../App.css";
+import MovingComponent from "react-moving-text";
+import headshot from "../images/headshot.jpeg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
 
 function About() {
   return (
-    <div className="about-component">
-      <img className="headshot" src={headshot} alt="Lauren Powers Head Shot"/>
-      <p className="about-branding-statement">
-I am a Software Engineer who is excited to build and create. My 12 years of experience in financial operations has helped me turn problem solving and crisis management into an everyday practice. My collaborative and determined approach enables me to see challenges as opportunities to learn and grow.</p>
+    <>
+      <div className="about-component">
+        <MovingComponent
+          type="fadeInFromLeft"
+          duration="2000ms"
+          delay="0s"
+          direction="normal"
+          timing="ease"
+          iteration="1"
+          fillMode="none"
+        >
+          <img
+            className="headshot"
+            src={headshot}
+            alt="Lauren Powers Head Shot"
+          />
+        </MovingComponent>
+        <div className="about-component">
+          <MovingComponent
+            type="fadeInFromRight"
+            duration="2000ms"
+            delay="0s"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none"
+          >
+            <p>
+              <a
+                href="https://www.linkedin.com/in/lauren-powers20/"
+                target="_blank"
+                className="about-me-icons"
+                rel="noreferrer"
+              >
+                <FaLinkedin alt="linkedin" />
+              </a>
+              <a
+                href="https://github.com/laurenpowers20"
+                target="_blank"
+                alt="github"
+                className="about-me-icons"
+                rel="noreferrer"
+              >
+                <FaGithub alt="github" />
+              </a>
+              <a href="" className="about-me-icons" alt="resume file">
+                <FaFileAlt alt="resume" />
+              </a>
+            </p>
+          </MovingComponent>
+        </div>
+
+        <MovingComponent
+          type="popIn"
+          duration="2000ms"
+          delay="0s"
+          direction="normal"
+          timing="ease"
+          iteration="1"
+          fillMode="none"
+        >
+          <p className="about-branding-statement">
+            I am a Software Engineer who is excited to build and create. My 12
+            years of experience in financial operations has helped me turn
+            problem solving and crisis management into an everyday practice. My
+            collaborative and determined approach enables me to see challenges
+            as opportunities to learn and grow.
+          </p>
+        </MovingComponent>
       </div>
-  )
+    </>
+  );
 }
 
-export default About
+export default About;
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,69 +1,93 @@
-import "../App.css"
-import lightning from "../images/lightning(1).png"
-import MovingComponent from 'react-moving-text'
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa"
-import { FaFileAlt } from "react-icons/fa"
+import "../App.css";
+import lightning from "../images/lightning(1).png";
+import MovingComponent from "react-moving-text";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+import { HiChevronDoubleRight } from "react-icons/hi2";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <div className="home-page"> 
-        
-        
-    <div className="im-lauren">
-    <MovingComponent
-      type="fadeInFromLeft"
-      duration="2000ms"
-      delay="0s"
-      direction="normal"
-      timing="ease"
-      iteration="1"
-      fillMode="none">
- <p> Hi, I'm Lauren Powers! <img className="lightning" src={lightning} alt="logo" /></p>
+      <div className="home-page">
+        <div className="im-lauren">
+          <MovingComponent
+            type="fadeInFromLeft"
+            duration="2000ms"
+            delay="0s"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none"
+          >
+           <p>
+              {" "}
+              Hi, I'm Lauren Powers!{" "}
+              <img className="lightning" src={lightning} alt="logo" />
+              </p>
           </MovingComponent>
-      </div>
-        
-      <div className="software-enigneer-title">    
-        <MovingComponent
-          // type="squeezeMix"
-          type="popIn"
-          duration="1000ms"
-          delay="0s"
-          direction="normal"
-          timing="ease"
-          iteration="1"
-          fillMode="none">
-          <div className="front-page-heading"><p>I'm a Software Engineer</p>
-          </div>
-          </MovingComponent>
-          </div>
+        </div>
 
-    
-    <div className="im-lauren">
-    <MovingComponent
-          type="fadeInFromRight"
-          duration="2000ms"
-          delay="0s"
-          direction="normal"
-          timing="ease"
-          iteration="1"
-          fillMode="none">
-            <p>
-              <a href="https://www.linkedin.com/in/lauren-powers20/" target="_blank" className="home-page-icons" rel="noreferrer">
-             <FaLinkedin alt="linkedin" /></a>
-              <a href="https://github.com/laurenpowers20" target="_blank" alt="github"className="home-page-icons" rel="noreferrer"><FaGithub alt="github" /></a>
+        <div className="software-enigneer-title">
+          <MovingComponent
+            // type="squeezeMix"
+            type="popIn"
+            duration="1000ms"
+            delay="0s"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none"
+          >
+            <div className="front-page-heading">
+              I'm a Software Engineer
+            </div>
+          </MovingComponent>
+        </div>
+
+        <div className="home-project-link">
+          <MovingComponent
+            type="fadeInFromRight"
+            duration="2000ms"
+            delay="0s"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none"
+          >
+              <p>
+              <NavLink className="navlink" to="/projects"><HiChevronDoubleRight /><p className="home-explore-work">Explore work</p> </NavLink>
+              </p>
+          
+              <p>
+              <a
+                href="https://www.linkedin.com/in/lauren-powers20/"
+                target="_blank"
+                className="home-page-icons"
+                rel="noreferrer"
+              >
+                <FaLinkedin alt="linkedin" />
+              </a>
+              <a
+                href="https://github.com/laurenpowers20"
+                target="_blank"
+                alt="github"
+                className="home-page-icons"
+                rel="noreferrer"
+              >
+                <FaGithub alt="github" />
+              </a>
               {/* <a href="" className="home-page-icons"> */}
               <FaFileAlt alt="resume" />
-            {/* </a> */}
+              {/* </a> */}
             </p>
+           
           </MovingComponent>
         </div>
-        </div>
-
-     
-      </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
