2c3eb78e794af17bbead65c3026f16edf8a17116
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-05 18:45:46 -0500

    edit about page

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -9,6 +9,7 @@ function About() {
   return (
     <>
       <div className="about-component">
+   
         <MovingComponent
           type="popIn"
           duration="2000ms"
@@ -23,44 +24,8 @@ function About() {
             src={headshot}
             alt="Lauren Powers Head Shot"
           />
-        </MovingComponent>
-        <div className="about-component">
-          <div className="about-icons">
-            <MovingComponent
-              type="fadeInFromRight"
-              duration="2000ms"
-              delay="0s"
-              direction="normal"
-              timing="ease"
-              iteration="1"
-              fillMode="none"
-            >
-              <a
-                href="https://www.linkedin.com/in/lauren-powers20/"
-                target="_blank"
-                className="about-me-icons"
-                rel="noreferrer"
-              >
-                <FaLinkedin alt="linkedin" />
-              </a>
-
-              <a
-                href="https://github.com/laurenpowers20"
-                target="_blank"
-                alt="github"
-                className="about-me-icons"
-                rel="noreferrer"
-              >
-                <FaGithub alt="github" />
-              </a>
-
-              <a href="" className="about-me-icons" alt="resume file">
-                <FaFileAlt alt="resume" />
-              </a>
-            </MovingComponent>
+          </MovingComponent>
           </div>
-        </div>
-
         <MovingComponent
           type="slideInFromBottom"
           duration="2000ms"
@@ -77,6 +42,40 @@ function About() {
             collaborative and determined approach enables me to see challenges
             as opportunities to learn and grow.
           </p>
+      </MovingComponent>
+      
+      <div className="about-icons">
+        <MovingComponent
+          type="fadeInFromRight"
+          duration="2000ms"
+          delay="0s"
+          direction="normal"
+          timing="ease"
+          iteration="1"
+          fillMode="none"
+        >
+          <a
+            href="https://www.linkedin.com/in/lauren-powers20/"
+            target="_blank"
+            className="about-me-icons"
+            rel="noreferrer"
+          >
+            <FaLinkedin alt="linkedin" />
+          </a>
+
+          <a
+            href="https://github.com/laurenpowers20"
+            target="_blank"
+            alt="github"
+            className="about-me-icons"
+            rel="noreferrer"
+          >
+            <FaGithub alt="github" />
+          </a>
+
+          <a href="" className="about-me-icons" alt="resume file">
+            <FaFileAlt alt="resume" />
+          </a>
         </MovingComponent>
       </div>
     </>
