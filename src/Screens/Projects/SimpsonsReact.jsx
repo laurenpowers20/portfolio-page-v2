b8f6cbd8c3544e03ff0ef7a98feaf67ebb49727e
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-05 12:34:43 -0500

    format simpsons project page

--- src/Screens/Projects/SimpsonsReact.jsx
+++ src/Screens/Projects/SimpsonsReact.jsx
@@ -1,32 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import { BsGlobe } from "react-icons/bs";
+import simpsons1 from "/Users/laurenpowers/Desktop/portfolio-page/src/images/simpsonscreenshot1.jpg";
+import simpsons2 from "/Users/laurenpowers/Desktop/portfolio-page/src/images/simpsonsscreenshot2.jpg";
 
 function SimpsonsReact() {
   return (
     <>
-      <h1>Simpsons</h1>
-      <a
-        href="https://github.com/laurenpowers20/simpsonsreact"
-        target="_blank"
-        rel="noreferrer"
-        className="project-link"
-      >
-        GitHub Repo
-      </a>
+      <main className="project-main-div">
+        <div className="project-descripton-images-container">
+          <img src={simpsons1} className="project-descripton-image" />
+          <img src={simpsons2} className="project-descripton-image" />
+        </div>
+        <div className="project-description">
+          <h1 className="project-heading">Simpsons Quote Generator</h1>
+          <button className="button1">
+            <a
+              href="https://github.com/laurenpowers20/simpsonsreact"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              GitHub Repo <FaGithub />
+            </a>
+          </button>
+          <button className="button1">
+            <a
+              href="https://simpsonsquotegenerator.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              Live Site <BsGlobe />
+            </a>
+          </button>
 
-      <a
-        href="https://simpsonsquotegenerator.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-        className="project-link"
-      >
-        Live Site
-      </a>
-
-      <p className="project-description-text">
-        A Simpsons Quote Generator built in React. All quotes and images pulled
-        from an API.
-      </p>
-      <Link to="/projects">Back to projects</Link>
+          <p className="project-description-text">
+            A Simpsons Quote Generator built in React. All quotes and images
+            pulled from an API.
+          </p>
+        </div>
+      </main>
+      <div className="back-to-projects-button-div">
+        <button className="button1">
+          <Link className="project-link" to="/projects">
+            Back to Projects
+          </Link>
+        </button>
+      </div>
     </>
   );
 }
