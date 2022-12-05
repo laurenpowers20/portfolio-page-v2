f10a273059790fd3ee3d857ce82b731bd9419a41
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-04 20:21:38 -0500

    add animation to projects

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,27 +1,25 @@
-import "./Styles/Nav.css"
-// import { stack as Menu } from 'react-burger-menu'
-import { NavLink } from "react-router-dom"
+import "./Styles/Nav.css";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
     <div className="nav-bar-component">
       <div className="nav-desktop">
-            <NavLink className="navlink" to="/">Home</NavLink>
-            <NavLink className="navlink" to="/about">About</NavLink>
-            <NavLink className="navlink" to="/projects">Projects</NavLink>
-            <NavLink className="navlink" to="/contact">Contact</NavLink>
-      </div> 
-
-      {/* <div className="nav-mobile">
-        <Menu>
-            <NavLink className="navlink" to="/">Home</NavLink>
-            <NavLink className="navlink" to="/about">About</NavLink>
-            <NavLink className="navlink" to="/projects">Projects</NavLink>
-            <NavLink className="navlink" to="/contact">Contact</NavLink>
-          </Menu>
-       </div>   */}
-       </div>
-  )
+        <NavLink className="navlink" to="/">
+          Home
+        </NavLink>
+        <NavLink className="navlink" to="/about">
+          About
+        </NavLink>
+        <NavLink className="navlink" to="/projects">
+          Projects
+        </NavLink>
+        <NavLink className="navlink" to="/contact">
+          Contact
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/Screens/Project.jsx
+++ src/Screens/Project.jsx
@@ -1,90 +1,101 @@
 import earth from "../images/earth.gif";
 import trivia from "../images/trivia.png";
 import simpsons from "../images/simpsons.png";
+import MovingComponent from "react-moving-text";
 import "../App.css";
 
 function Projects() {
   return (
     <>
       <h1 className="page-heading">Projects</h1>
-      {/* <p className="about-branding-statement">Page under construction</p>
-      <p className="about-branding-statement">Come visit soon to see my projects. For now, check out my <a href="https://github.com/laurenpowers20" target="_blank" alt="github" rel="noreferrer">GitHub page</a></p> */}
 
-      <main className="project-section">
-        <div className="projects-container">
-          <img src={earth} alt="worldmap" className="project-image" />
-          <p className="project-description-title">
-            Countries of The World API
-          </p>
-          <a
-            href="https://github.com/laurenpowers20/jsonapi"
-            target="_blank"
-            rel="noreferrer"
-            className="project-link"
-          >
-            GitHub Repo
-          </a>
-          <p className="project-description-text">
-            A JSON API built with Mongoose, Express, and JavaScript.
-          </p>
-        </div>
+      <MovingComponent
+        type="slideInFromTop"
+        duration="1000ms"
+        delay="0s"
+        direction="normal"
+        timing="ease"
+        iteration="1"
+        fillMode="none"
+      >
+        <main className="project-section">
+          <div className="projects-container">
+            <img src={earth} alt="worldmap" className="project-image" />
+            <p className="project-description-title">
+              Countries of The World API
+            </p>
+            <a
+              href="https://github.com/laurenpowers20/jsonapi"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              GitHub Repo
+            </a>
+            <p className="project-description-text">
+              A JSON API built with Mongoose, Express, and JavaScript.
+            </p>
+          </div>
 
-        <div className="projects-container">
-          <img src={trivia} alt="worldmap" className="project-image" />
+          <div className="projects-container">
+            <img src={trivia} alt="worldmap" className="project-image" />
 
-          <p className="project-description-title">Game of Trivia</p>
-          <a
-            href="https://github.com/laurenpowers20/trivia"
-            target="_blank"
-            rel="noreferrer"
-            className="project-link"
-          >
-            GitHub Repo
-          </a>
+            <p className="project-description-title">Game of Trivia</p>
+            <a
+              href="https://github.com/laurenpowers20/trivia"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              GitHub Repo
+            </a>
 
-          <a
-            href="https://triviagame1022.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="project-link"
-          >
-            Live Site
-          </a>
+            <a
+              href="https://triviagame1022.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              Live Site
+            </a>
 
-          <p className="project-description-text">
-            A multiple choice game of trivia written in HTML, CSS, and
-            JavaScript.
-          </p>
-        </div>
+            <p className="project-description-text">
+              A multiple choice game of trivia written in HTML, CSS, and
+              JavaScript.
+            </p>
+          </div>
 
-        <div className="projects-container">
-          <img src={simpsons} alt="worldmap" className="project-image" />
+          <div className="projects-container">
+            <img src={simpsons} alt="worldmap" className="project-image" />
 
-          <p className="project-description-title">Simpsons Quote Generator</p>
-          <a
-            href="https://github.com/laurenpowers20/simpsonsreact"
-            target="_blank"
-            rel="noreferrer"
-            className="project-link"
-          >
-            GitHub Repo
-          </a>
+            <p className="project-description-title">
+              Simpsons Quote Generator
+            </p>
+            <a
+              href="https://github.com/laurenpowers20/simpsonsreact"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              GitHub Repo
+            </a>
 
-          <a
-            href="https://simpsonsquotegenerator.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="project-link"
-          >
-            Live Site
-          </a>
+            <a
+              href="https://simpsonsquotegenerator.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-link"
+            >
+              Live Site
+            </a>
 
-          <p className="project-description-text">
-            A Simpsons Quote Generator built in React. All quotes and images
-            pulled from an API.
-          </p>
-        </div>
-      </main>
+            <p className="project-description-text">
+              A Simpsons Quote Generator built in React. All quotes and images
+              pulled from an API.
+            </p>
+          </div>
+        </main>
+      </MovingComponent>
     </>
   );
 }
