d329eb783f0a6c51d86d7b58c5591f564b835504
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-01-03 16:59:04 -0500

    edit meta tag

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -1,8 +1,27 @@
 import { FaLinkedin } from "react-icons/fa";
 import { TfiEmail } from "react-icons/tfi";
 import MovingComponent from "react-moving-text";
+// import React, { useState } from "react";
 
 function Contact() {
+  // const [form, setForm] = useState({
+  //   email: "",
+  //   message: "",
+  // });
+
+  // const handleInputChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setForm((prevProps) => ({
+  //     ...prevProps,
+  //     [name]: value,
+  //   }));
+  // };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(form);
+  // };
+
   return (
     <>
       <h1 className="page-heading">Contact</h1>
@@ -15,6 +34,32 @@ function Contact() {
         iteration="1"
         fillMode="none"
       >
+        {/* <form onSubmit={handleSubmit}>
+          <div>
+            <label for="email" className="contact-me">
+              {" "}
+            </label>
+            <input
+              className="contact-email"
+              type="email"
+              id="email"
+              placeholder="E-mail"
+              name="email"
+              value={form.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label className="contact-me">
+              <textarea type="textarea" placeholder="Message" />
+            </label>
+          </div>
+
+          <label></label>
+          <button className="button1" type="submit">
+            Login
+          </button>
+        </form> */}
         <p className="contact-me">
           {" "}
           Find me on{" "}
