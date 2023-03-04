9cb403e86f653b25af9e2c4f128c43449d5e7cf9
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-04 14:16:11 -0500

    add cool buttons to nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,17 +5,18 @@ function Nav() {
   return (
     <div className="nav-bar-component">
       <div className="nav-desktop">
+        {" "}
         <NavLink className="navlink" to="/">
-          Home
+          <button className="button-74"> Home </button>
         </NavLink>
         <NavLink className="navlink" to="/about">
-          About
+          <button className="button-74">About</button>
         </NavLink>
         <NavLink className="navlink" to="/projects">
-          Projects
+          <button className="button-74"> Projects</button>
         </NavLink>
         <NavLink className="navlink" to="/contact">
-          Contact
+          <button className="button-74"> Contact</button>
         </NavLink>
       </div>
     </div>
